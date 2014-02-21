235479797aef159c88eba116f205ce2796381a89
Nick <nick@nicks-air.btc-adsl>
2014-02-21 08:15:33 +0200

    tutorial

--- Countdown/ViewController.h
+++ Countdown/ViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UILabel *countdownLabel;
 
+- (IBAction)startCountdown:(id)sender;
 @end
--- Countdown/ViewController.m
+++ Countdown/ViewController.m
@@ -26,4 +26,32 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)startCountdown:(id)sender {
+    //Remove the time component from the datePicker.  We care only about the date
+    NSCalendar *calendar = [NSCalendar autoupdatingCurrentCalendar];
+    NSUInteger preservedComponents = (NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit);
+    self.datePicker.date = [calendar dateFromComponents:[calendar components:preservedComponents fromDate:self.datePicker.date]];
+
+    //Set up a timer that calls the updateTime method every second to update the label
+    NSTimer *timer;
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0
+                                             target:self
+                                           selector:@selector(updateTime)
+                                           userInfo:nil
+                                            repeats:YES];
+}
+
+-(void)updateTime
+{
+    //Get the time left until the specified date
+    NSInteger ti = ((NSInteger)[self.datePicker.date timeIntervalSinceNow]);
+    NSInteger seconds = ti % 60;
+    NSInteger minutes = (ti / 60) % 60;
+    NSInteger hours = (ti / 3600) % 24;
+    NSInteger days = (ti / 86400);
+    
+    //Update the lable with the remaining time
+    self.countdownLabel.text = [NSString stringWithFormat:@"%02i days %02i hrs %02i min %02i sec", days, hours, minutes, seconds];
+}
+
 @end
