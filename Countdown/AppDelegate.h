0a3ad458b817d8826e6542cd9a96dee39e34eca0
Nick <nick@nicks-air.btc-adsl>
2014-02-21 05:19:15 +0200

    Initial Commit

---
+++ Countdown/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Countdown
+//
+//  Created by Nick on 2/21/14.
+//  Copyright (c) 2014 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Countdown/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Countdown
+//
+//  Created by Nick on 2/21/14.
+//  Copyright (c) 2014 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
